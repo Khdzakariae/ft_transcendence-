e6499ea15778b31d144d982f990beedd47b8ab4f
Zakariae El-Khadir <zel-khad@e1r9p2.1337.ma>
2025-10-10 11:55:40 +0100

    backend

--- srcs/front/tools/src/pages/SignIn.tsx
+++
@@ -1,7 +0,0 @@
-import { LoginPage } from "@/components/login/LoginPage";
-
-const SignIn = () => {
-  return <LoginPage />;
-};
-
-export default SignIn;
--- srcs/user-service/tools/routes/notification.routes.js
+++
@@ -1,7 +0,0 @@
-import { getNotifications, markRead, markAllRead } from '../controllers/notification.controller.js';
-
-export default async function notificationRouter(fastify, opts) {
-  fastify.get('/', getNotifications);
-  fastify.post('/mark-read', markRead);
-  fastify.post('/mark-all-read', markAllRead);
-}
