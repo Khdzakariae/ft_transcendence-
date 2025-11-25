0d07425c61d9da4c17f83f4176c95df1cc560574
hicham essaquy <hichamessaquy@hichams-MacBook-Pro.local>
2025-11-25 23:02:12 +0100

    setting up game ui

--- srcs/front/tools/src/components/dashboard-sections/GameSection.tsx
+++ srcs/front/tools/src/components/dashboard-sections/GameSection.tsx
@@ -9,12 +9,14 @@ const canvasRef = useRef<HTMLCanvasElement | null>(null);
         // making ccs dims match canvas dims.
         //const rect = canvas.getBoundingClientRect();
     
-        canvas.width = 800;
-        canvas.height = 400;
+        const rect = canvas.getBoundingClientRect();
+
+        canvas.width = rect.width * window.devicePixelRatio;
+        canvas.height = rect.height * window.devicePixelRatio;
         const ctx = canvas.getContext("2d");
-        if (!ctx)
+        if  (!ctx)
             return;
-        //ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+        ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
         ctx.beginPath();
         ctx.fillStyle = "red";
         ctx.rect(0, 0 , canvas.width,  canvas.height);
@@ -23,10 +25,21 @@ const canvasRef = useRef<HTMLCanvasElement | null>(null);
     },[]);
 
   return (
-    <div>
-      <canvas
-        ref={canvasRef}
-      />
-    </div>
+<div className="min-h-screen bg-primary-bg flex items-center justify-center w-full text-center">
+
+    {/* Top overlay  : add some game functionality to this*/}
+    <div className="absolute top-0 left-0 w-full flex items-center justify-center z-10 pointer-events-none">
+      <p className="text-white text-xl font-bold text-center">Game Functions</p>
+  </div>
+    {/* Canvas for game*/}
+    <canvas
+      ref={canvasRef}
+      className="block bg-primary-elements p-4 sm:p-6 md:p-8 rounded-lg w-full border border-white/10"
+    />
+
+</div>
+
   );
 }
+
+
